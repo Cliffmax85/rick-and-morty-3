5aaa25a5ea4d0c75779a62cf0a8753779bbe24b5
cliffmax85 <cliffmax85@gmail.com>
2022-05-11 17:45:58 -0700

    added card component, and dropdown filter

--- src/views/Characters/List.jsx
+++ src/views/Characters/List.jsx
@@ -1,19 +1,34 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import Card from '../../components/Card';
 
 export default function List() {
     const [character, setCharacter] = useState([]);
     const [loading, setLoading] = useState(true);
+    const history = useHistory();
+    const location = useLocation();
+    const status = new URLSearchParams(location.search).get('status') ?? 'all';
 
-
+    console.log('LOG CHAR', character);
     useEffect(() => {
     async function fetchCharacters() {
-        const res = await fetch('https://rickandmortyapi.com/api/character');
-        const { results } = await res.json();
-        setCharacter(results);
+        setLoading(true);
+
+        const search = new URLSearchParams(location.search);
+        const params = search.get('status') ?? 'all';
+
+        const url =
+          params === 'all' ? 'https://rickandmortyapi.com/api/character' : `https://rickandmortyapi.com/api/character?status=${status}`;
+
+        const res = await fetch(url);
+        const data = await res.json();
+        setCharacter(data.results);
         setLoading(false);
     };
     fetchCharacters();
-    }, []);
+    }, [location.search]);
 
     return (
         <>
@@ -21,15 +36,28 @@ export default function List() {
           {loading ? (
               <h3>Loading!</h3>
           ) : (
-              <>
+              <section>
+                <label htmlFor="status">
+                    Search By Character Status
+                </label>
+                <select name=''
+                    id='status'
+                    value={status}
+                    onChange={(e) => history.push(`/?status=${e.target.value}`)}>
+                    <option value='all'>All</option>
+                    <option value='alive'>Alive</option>
+                    <option value='dead'>Dead</option>
+                    <option value='unkown'>Unkown</option>
+                </select>
                 <div className="card">
-                    {character.map((char) => 
-                        <section key={char.id}>
-                        <p>{char.name}</p>
-                        </section>
+                    {character.map((character) => (
+                        <Link to={`/character/${character.id}`} key={character.id + character.name}>
+                            <Card character={character} />
+                        </Link>
+                    )
                     )}
                 </div>
-              </>
+              </section>
           )}
         </>
     )
